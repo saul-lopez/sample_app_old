46c15fac7b882327314c184b1a61198287b59a92
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-08-20 18:45:06 -0500

    Uso de rutas con nombre. Se eliminar index.html para que se pueda acceder sin problemas al home de la aplicación.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,57 +5,62 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+    it "should have the h1 'Sample App'" do
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
     end
 
-    it "should have the right title" do
-      visit '/static_pages/home'
+    it "should have the base title" do
+      visit root_path
       page.should have_selector('title',
-                                :text => "#{base_title}")
+                                text: "#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
+      visit root_path
+      page.should_not have_selector('title', text: '| Home')
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+
+    it "should have the h1 'Help'" do
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
 
-    it "should have the right title" do
-      visit '/static_pages/help'
+    it "should have the title 'Help'" do
+      visit help_path
       page.should have_selector('title',
-                                :text => "#{base_title} | Help")
+                                text: "#{base_title} | Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+
+    it "should have the h1 'About'" do
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
-    it "should have the right title" do
-      visit '/static_pages/about'
+
+    it "should have the title 'About Us'" do
+      visit about_path
       page.should have_selector('title',
-                                :text => "#{base_title} | About Us")
+                                text: "#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
     end
-    it "should have the right title" do
-      visit '/static_pages/contact'
+
+    it "should have the title 'Contact'" do
+      visit contact_path
       page.should have_selector('title',
-                                :text => "#{base_title} | Contact")
+                                text: "#{base_title} | Contact")
     end
   end
 end
