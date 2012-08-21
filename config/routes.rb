04f1c4c83f4e11c58e20b133c23faeeaf7e72041
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-08-20 20:08:59 -0500

    Esqueleto para alta de usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
+  match '/signup', to: 'user#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
