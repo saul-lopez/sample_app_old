5738cc6b0be3fe23ff2f171c980ea021c669de5f
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-08-24 00:00:00 -0500

    Campos "virtuales" para password y confirmación. Pruebas de unidad. Modificación del modelo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,25 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  ## Con este metodo, automágicamente se implementa el método authenticate,
+  ## Breve explicación: http://ruby.railstutorial.org/chapters/modeling-users?version=3.2#sec-has_secure_password
+  has_secure_password
   # Hook para antes de guardar, convertir a minusculas
   before_save { |user| user.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # Se valida unicidad, pero de cualquier forma es importante crear el unique a nivel base de datos
   # ver http://ruby.railstutorial.org/chapters/modeling-users?version=3.2#sec-the_caveat
   # El indice se crea en db/migrate/x_add_index_to_users_email
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX  }        ,
                     uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120824002111) do
+ActiveRecord::Schema.define(:version => 20120824034205) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
     t.string   "password_digest"
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
