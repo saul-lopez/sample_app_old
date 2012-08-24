baf05bed3a95ad3f43cf2da1df0f6a8770c0c89f
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-08-23 22:32:11 -0500

    Columna para el password "hasheado". Prueba de unidad para existencia de campo

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,7 @@ describe User do
   subject { @user }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
   it { should be_valid }
   describe "when name is not present" do
     before { @user.name = "     " }
