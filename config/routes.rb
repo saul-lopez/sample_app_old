6c9fc41a69b43ba35e171cc565da9b456a52ca78
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-09-05 00:24:52 -0500

    Definición de rutas y acciones para sign-in-out, pruebas de unidad para la página signin

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,15 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
-  match '/signup', to: 'users#new'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
