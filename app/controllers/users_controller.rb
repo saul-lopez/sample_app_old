b72648701957e7e65262e73d3c9102fb005d0384
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-09-10 22:43:19 -0500

    Iniciar sesion. Pruebas de unidad

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
       #Handle a successfull update.
     else
       render 'edit'
