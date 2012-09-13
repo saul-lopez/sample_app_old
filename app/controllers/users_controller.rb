d58f19cc2d738a5de1437b11a168aad0e15c9cd9
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-09-13 16:58:16 -0500

    Permitir eliminar usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
   before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -34,6 +35,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
   private
     def signed_in_user
       unless signed_in?
@@ -47,5 +53,8 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_path, notice: "Intento editar un usuario distinto. No mameye." unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to root_path unless current_user.admin?
     end
 end
