cc27707ef5e0d2a060e9492461ef6a85943c5469
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-09-14 00:19:22 -0500

    Ejercicio 9.6.8. Prueba para asegurarnos que la ruta de return_to sea eliminada una vez que se inicia sesión. Es decir:  si un usuario intenta editar su perfil sin tener sesión iniciada, al iniciarla será redirigido al formulario de edición de perfil. Pero si inicia sesión nuevamente, será redirigido al visor de perfil, y ya no al editor.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -56,6 +56,18 @@ describe "Authentication" do
             page.should have_selector('title', text: 'Edit user')
           end
         end
+        describe "when signing in again" do
+          before do
+            visit signin_path
+            fill_in "Email",    with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
+          end
+
+          it "should render the default (profile) page" do
+            page.should have_selector('title', text: user.name)
+          end
+        end
       end
 
       describe "submitting to the update action" do
