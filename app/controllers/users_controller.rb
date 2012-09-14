9a1bdb8299e03d9818e9f457ccef837e2844a0f7
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-09-14 17:05:08 -0500

    Requerir autenticación para create y destroy de microposts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,15 +43,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   private
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-      # El anterior redirect es equivalente a:
-      # flash[:notice] = "Please sign in."
-      # redirect_to signin_url
-    end
     def signed_in_user_donts
       redirect_to root_path unless !signed_in?
     end
