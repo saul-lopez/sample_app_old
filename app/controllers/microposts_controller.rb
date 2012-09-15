3a0f16760bfccc44540295590d510a03c1f3a556
Saul Lopez <saul.lopez@quixmati.com.mx>
2012-09-14 19:04:50 -0500

    Mostrar microposts en la página de inicio.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,9 @@ class MicropostsController < ApplicationController
       flash[:success]  = "Micropost created!"
       redirect_to root_url
     else
+      # static_pages/home espera una lista de microposts
+      # así que Si se falla al guardar un micropost, enviar una lista vacía
+      @feed_items = []
       render 'static_pages/home'
     end
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if signed_in?
+    if signed_in?
+      # Instancia de micropost a usarse en el formulario
+      @micropost = current_user.microposts.build if signed_in?
+      # Lista de microposts previos
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
